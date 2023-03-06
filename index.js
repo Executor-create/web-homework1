6333c1d55d84f3a25ea5a97ab2803d4235657e22
Executor <just4dotaalex@gmail.com>
2023-03-06 18:26:21 +0200

    feat: add theater router

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config();
+const theaterRouter = require('./api/theater/getTheater');
 
 const app = express();
 
+const setup = require('./DB/mongoose');
+
+app.use(bodyParser.json());
+app.use(theaterRouter);
+
 app.listen(process.env.PORT, () => {
-  console.log("Server started");
+  console.log(`Server started ${process.env.PORT}`);
 });
+
+setup();
