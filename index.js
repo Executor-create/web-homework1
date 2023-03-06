a40b54a6c92d8e7b0bb9da07b71d840f3874361c
Executor <just4dotaalex@gmail.com>
2023-03-06 19:56:32 +0200

    feat: add get session router

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 const usersRouter = require('./api/user/getUsers');
 const theaterRouter = require('./api/theater/getTheater');
+const sessionRouter = require('./api/session/getSessions');
 
 const app = express();
 
@@ -11,6 +12,7 @@ const setup = require('./DB/mongoose');
 app.use(bodyParser.json());
 app.use(usersRouter);
 app.use(theaterRouter);
+app.use(sessionRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started ${process.env.PORT}`);
